a84dc76a27d34eca0c54fb41d5991759d510d604
Rasel5267 <mahadiul09@gmail.com>
2020-07-17 23:50:41 +0600

    finish project

---
+++ assignment.js
@@ -0,0 +1,54 @@
+// Feet To Mile Conveert
+function feetToMile(feet){
+    if (feet == 0){
+        return 0;
+    }
+    else if(feet > 0){
+        return feet * 0.000189394;
+    }
+    else{
+        return "Length can not be negetive."
+    }
+}
+
+
+//Wood Calculator
+function woodCalculator(chair, table, bed){
+    var chairCount = chair * 1;
+    var tableCount = table * 3;
+    var bedCount = bed * 5;
+    var totalCount = chairCount + tableCount + bedCount;
+    return totalCount;
+}
+
+
+//tiny Friends
+function tinyFriend(names){
+    var tiny = names[0];
+    for(var i =0; i< names.length; i++){
+        var currentName = names[i];
+        if(currentName.length < tiny.length){
+            tiny = currentName;
+        }
+    }
+    return tiny;
+}
+
+//Brick calculator
+function brickCalculator(floor){
+    if(floor <=10){
+        var feet1 = floor * 15;
+        var brick = feet1 * 1000;       
+    }
+    else if(floor>=11 && floor<=20){
+        var feet2 = floor * 12;
+        var brick = feet2 * 1000;
+    }
+    else{
+        var feet3 = floor * 10;
+        var brick = feet3 * 1000;
+    }
+    return brick;
+}
+var result = brickCalculator(15);
+console.log(result);
